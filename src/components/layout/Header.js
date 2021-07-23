e3caea4c4e8eb0c3a9c6e8370bf0973b95c15939
daniel <daniel21dev@gmail.com>
2021-07-22 19:32:33 -0500

    rutas publicas y privadas

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom'
 export const Header = () => {
     return (
         <header className="w-full h-12 bg-gray-800 text-white shadow-2xl px-4 py-1 flex justify-between">
-            <h3 className="text-3xl cursor-pointer">App</h3>
+            <Link to='/' className="text-3xl cursor-pointer"
+            >App</Link>
             <ul className="flex items-center">
                 <li className="mx-2 hover:text-green-500">
                     <Link to='/'>Home</Link>
